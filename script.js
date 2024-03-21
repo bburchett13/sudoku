2c11e00d7762ac35884a1810c31c6529e994b9e4
Blake <bburchett13@gmail.com>
2024-03-21 19:04:23 -0400

    Added logic to check user's answers

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
 let selectedNum = 1;
 var gameCells = [];
+var gameLayout = [];
+gameLayout = [5, 3, 4, 6, 7, 8, 9, 1, 2, '', 7, 2, 1, 9, 5, 3, 4, '', 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1, 4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9, 6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9];
+var answer = [5, 3, 4, 6, 7, 8, 9, 1, 2, 6, 7, 2, 1, 9, 5, 3, 4, 8, 1, 9, 8, 3, 4, 2, 5, 6, 7, 8, 5, 9, 7, 6, 1, 4, 2, 3, 4, 2, 6, 8, 5, 3, 7, 9, 1, 7, 1, 3, 9, 2, 4, 8, 5, 6, 9, 6, 1, 5, 3, 7, 2, 8, 4, 2, 8, 7, 4, 1, 9, 6, 3, 5, 3, 4, 5, 2, 8, 6, 1, 7, 9];
+console.log(gameLayout.length);
+
+// Create buttons for each cell and provide game logic
+//#region
 for (let i = 0; i < 81; i++) {
-
-
+    
+    // Find game cell
     gameCells[i] = document.getElementsByClassName('gameCell')[i];
-    gameCells[i].addEventListener('click', () => {
+    
+    // create game board using the gameLayout
+    gameCells[i].innerHTML = gameLayout[i];
 
-        gameCells[i].innerHTML = selectedNum;
+    // If statement to create button for playable cells and decide what to do when the user puts a number in place
+    if (gameLayout[i] === '') {
 
-    });
+        gameCells[i].classList.add = 'playableCell';
+        gameCells[i].style.backgroundColor = 'white';
+        gameCells[i].addEventListener('click', () => {
+    
+            gameCells[i].innerHTML = selectedNum;
 
+            // if wrong, make cell red, if erase, make cell white, if correct, make cell green
+            if (selectedNum !== answer[i] && selectedNum !== '') {
+
+                gameCells[i].style.backgroundColor = 'red';
+
+            }
+            else if (selectedNum === '') {
+
+                gameCells[i].style.backgroundColor = 'white';
+
+            }
+            else {
+
+                gameCells[i].style.backgroundColor = 'green';
+
+            }
+    
+        });
+
+    };
+    
 };
+//#endregion
 
-// let gameCell = document.querySelector("#cell1");
-// gameCell.addEventListener('click', () => {
 
-//     gameCell.innerHTML = selectedNum;
-
-// });
-
-// let gameCell2 = document.getElementsByClassName('gameCell')[1]
-//         gameCell2.addEventListener('click', function () {
-//             gameCell2.innerHTML = selectedNum;
-//         });
-
-// Make Number Buttons and Assign them to selectedNum on click
 //#region
 let btn1 = document.querySelector("#button1");
 btn1.addEventListener('click', () => {
@@ -86,6 +110,13 @@ let btn9 = document.querySelector("#button9");
 btn9.addEventListener('click', () => {
 
     selectedNum = 9;
+
+});
+let btnErase = document.querySelector("#erase");
+btnErase.addEventListener('click', () => {
+
+    selectedNum = '';
+
 
 });
 //#endregion
